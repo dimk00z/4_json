34ad44399c0cf3d091b2ed5d637da667d5f4b5c7
Dmitry <dimk00z@gmail.com>
2017-01-15 20:39:05 +0300

    правки переменных и функций

--- pprint_json.py
+++ pprint_json.py
@@ -2,7 +2,8 @@ import json
 import os
 import argparse
 
-def load_data(filepath):
+
+def load_file(filepath):
     if not os.path.exists(filepath):
         print("Файла " + filepath + " не найдено!")
         return None
@@ -10,7 +11,8 @@ def load_data(filepath):
         print("Загружен файл " + filepath)
         return open(filepath, 'r', encoding='utf-8')
 
-def load_json(json_file):
+
+def load_json_data(json_file):
     try:
         return json.load(json_file)
     except ValueError:
@@ -21,13 +23,18 @@ def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
+def read_json_filenames_from_args():
+    args_parser = argparse.ArgumentParser(add_help=False)
+    args_parser.add_argument("json_file_name", type=str, nargs='+',
+                             help="json file for pretty print")
+    return args_parser.parse_args().json_file_name
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("json_file_name", type=str, nargs='+',
-                        help="display a square of a given number")
-    for arg in parser.parse_args().json_file_name:
-        json_from_file = load_data(arg)
-        json_data = load_json(json_from_file)
+    json_file_names = read_json_filenames_from_args()
+    for json_file_name in json_file_names:
+        file_data = load_file(json_file_name)
+        json_data = load_json_data(file_data)
         if json_data:
             pretty_print_json(json_data)
     print("Программа завершена.")
