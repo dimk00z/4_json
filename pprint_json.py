828d799f8ffe9304b6e696070ab6e44bad072fa6
Dmitry <dimk00z@gmail.com>
2017-01-17 14:00:04 +0300

    очередные правки

--- pprint_json.py
+++ pprint_json.py
@@ -1,7 +1,7 @@
 import json
 import os
 import argparse
-
+from json import dumps,load as json_load 
 
 def load_file(filepath):
     if os.path.exists(filepath):
@@ -10,14 +10,14 @@ def load_file(filepath):
 
 def load_json_data(json_file):
     try:
-        return json.load(json_file)
+        return json_load(json_file)
     except ValueError:
         return None
 
 
-def pretty_print_json(data):
-    if data:
-        print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(json):
+    if json:
+        print(dumps(json, indent=4, sort_keys=True, ensure_ascii=False))
     else:
         print("Ошибка переобразования JSON файла")
 
@@ -29,17 +29,14 @@ def read_json_filenames_from_args():
     return args_parser.parse_args().json_file_name
 
 
-def print_file_load_result(json_file_name, file_data):
-    if not file_data:
-        print("Файла {} не найдено!".format(json_file_name))
-    else:
-        print("Загружен файл {}".format(json_file_name))
-
 if __name__ == '__main__':
     json_file_names = read_json_filenames_from_args()
     for json_file_name in json_file_names:
         file_data = load_file(json_file_name)
-        print_file_load_result(json_file_name, file_data)
-        json_data = load_json_data(file_data)
-        pretty_print_json(json_data)
-    print("Программа завершена.")
+        if not file_data:
+            print("Файла {} не найдено!".format(json_file_name))
+        else:
+            print("Загружен файл {}".format(json_file_name))
+            json = load_json_data(file_data) 
+            pretty_print_json(json)           
+        print("Программа завершена.")
